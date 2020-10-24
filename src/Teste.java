e49fab1c3589635a1b06b2acb460319fc155f518
zeus <dhiogo.correa@itera.com.br>
2020-10-24 15:38:37 -0300

    changed teste main function

--- src/Teste.java
+++ src/Teste.java
@@ -1,4 +1,5 @@
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
@@ -7,10 +8,17 @@ import com.grupo30.model.Peso;
 
 public class Teste {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		DateFormat dtf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		PesoDao pesoDao = new PesoDao();
+		
+		pesoDao.insert(new Peso(82.0, dtf.parse("10/09/2020")));
+		pesoDao.insert(new Peso(80.0, dtf.parse("26/09/2020")));
+		pesoDao.insert(new Peso(78.0, dtf.parse("05/10/2020")));
+		pesoDao.insert(new Peso(77.0, dtf.parse("10/10/2020")));
+		pesoDao.insert(new Peso(76.0, dtf.parse("20/10/2020")));
+		
 		List<Peso> pesos = pesoDao.getAll();
 		
 		for(Peso peso : pesos) {
